7aebcd21fbd2c7a88b10d82dc2f841da3729e872
gowjr207@gmail.com <gowjr207@gmail.com>
2022-12-21 01:28:09 +0900

    [STYLE] 유레카 Enable 설정

--- gateway/src/main/java/com/luckytree/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/luckytree/gateway/GatewayApplication.java
@@ -2,8 +2,10 @@ package com.luckytree.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
 
